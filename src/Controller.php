d389314203d6b3487b07f063d18d9399ae455ce2
1D4n3 <125080408+1D4n3@users.noreply.github.com>
2024-01-10 17:08:11 +0300

    Task03

--- src/Controller.php
+++ src/Controller.php
@@ -1,10 +1,40 @@
 <?php
 
-namespace 1D4n3\GuessNumber\Controller;
-use function 1D4n3\GuessNumber\View\showGame;
-use function cli\line;
+namespace Idane\GuessNumber\Controller;
 
-function startGame()
+use function Idane\GuessNumber\View\displayGame;
+use function Idane\GuessNumber\View\displayWelcomeMessage;
+use function Idane\GuessNumber\Model\generateNumber;
+use function cli\line;
+use function cli\input;
+
+define("MAX_VALUE", 500);
+define("MAX_ATTEMPTS", 15);
+
+function gameLoop()
 {
-    line("Угадай число!");
+    $attempt = 0;
+    $generated_number = generateNumber();
+    while ($attempt < MAX_ATTEMPTS) {
+        $user_input = input();
+        if ($user_input == $generated_number) {
+            line("Поздравляем! Вы угадали число");
+            return;
+        } elseif ($user_input === "!") {
+            line("Вы вышли из игры");
+            return;
+        } elseif ($user_input > $generated_number) {
+            line("Загаданное число меньше");
+        } else {
+            line("Загаданное число больше");
+        }
+        $attempt++;
+    }
+    line("Попытки исчерпаны");
+}
+function initiateGame()
+{
+    displayWelcomeMessage();
+    displayGame();
+    gameLoop();
 }
