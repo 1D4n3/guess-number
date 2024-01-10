5bbb6daf84f5ce2908f2ed0119430804f49b8753
1D4n3 <125080408+1D4n3@users.noreply.github.com>
2024-01-10 16:35:06 +0300

    Add files via upload

---
+++ src/Controller.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace 1D4n3\GuessNumber\Controller;
+use function 1D4n3\GuessNumber\View\showGame;
+use function cli\line;
+
+function startGame()
+{
+    line("Угадай число!");
+}
---
+++ src/View.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace 1D4n3\GuessNumber\View;
+use function cli\line;
+
+function showGame()
+{
+    line("Интерфейс игры");
+}
